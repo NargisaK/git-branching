477265ca4ddb6de128f16fd279055ddefb6c7098
mariia <m.le13@yahoo.com>
2023-09-12 20:36:57 -0500

    updated login page

--- src/main/java/com/academy/techcenture/config/constants/pages/LoginPage.java
+++ src/main/java/com/academy/techcenture/config/constants/pages/LoginPage.java
@@ -1,4 +1,9 @@
 package com.academy.techcenture.config.constants.pages;
 
 public class LoginPage {
-}
+
+        public void login(){
+            System.out.println("Login successful");
+        }
+    }
+
