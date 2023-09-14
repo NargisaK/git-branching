e97d446e4f5b17a3e71956354a1558ee87e75802
abbos <tabbos36@gmail.com>
2023-09-14 17:23:29 -0400

    Poem

--- src/main/java/com/academy/techcenture/config/constants/pages/ViewAllProductsPage.java
+++ src/main/java/com/academy/techcenture/config/constants/pages/ViewAllProductsPage.java
@@ -1,7 +1,29 @@
 package com.academy.techcenture.config.constants.pages;
 
+import com.github.javafaker.Faker;
+
 public class ViewAllProductsPage {
     public static void main(String[] args) {
         System.out.println("hi hi hi hi ");
+    }
+    public void randomMessage(){
+
+        System.out.println("Is that Eric Garner worked\n" +
+                "for some time for the Parks and Rec.\n" +
+                "Horticultural Department, which means,\n" +
+                "perhaps, that with his very large hands,\n" +
+                "perhaps, in all likelihood,\n" +
+                "he put gently into the earth\n" +
+                "some plants which, most likely,\n" +
+                "some of them, in all likelihood,\n" +
+                "continue to grow, continue\n" +
+                "to do what such plants do, like house\n" +
+                "and feed small and necessary creatures,\n" +
+                "like being pleasant to touch and smell,\n" +
+                "like converting sunlight\n" +
+                "into food, like making it easier\n" +
+                "for us to breathe.\n" +
+                "\n");
+
     }
 }
